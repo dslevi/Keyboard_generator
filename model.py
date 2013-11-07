b9f354eb801ae27f5fa6415356e3b07fa0a933d1
Hackbright Student <user@chromebox08.(none)>
2013-11-06 18:32:23 -0800

    Added more fields to registration form, halfway through prompt cycling

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, ForeignKey
-from sqlalchemy import Column, Integer, String, DateTime, Text
+from sqlalchemy import Column, Integer, String, DateTime, Text, Boolean
 
 from sqlalchemy.orm import sessionmaker, scoped_session, relationship, backref
 
@@ -21,11 +21,14 @@ Base.query = session.query_property()
 class User(Base, UserMixin):
     __tablename__ = "users" 
     id = Column(Integer, primary_key=True)
+    name = Column(String(64), nullable=False)
     email = Column(String(64), nullable=False)
     password = Column(String(64), nullable=False)
     salt = Column(String(64), nullable=False)
-
-    posts = relationship("Post", uselist=True)
+    analytics = relationship("Analytics", uselist=True)
+    right_hand = Column(String(10), nullable=False)
+    occupation = Column(String(64), nullable=False)
+    age = Column(Integer)
 
     def set_password(self, password):
         self.salt = bcrypt.gensalt()
@@ -36,27 +39,40 @@ class User(Base, UserMixin):
         password = password.encode("utf-8")
         return bcrypt.hashpw(password, self.salt.encode("utf-8")) == self.password
 
-class Post(Base):
-    __tablename__ = "posts"
-    
+class Analytics(Base):
+    __tablename__ = "analytics"
+
     id = Column(Integer, primary_key=True)
-    title = Column(String(64), nullable=False)
-    body = Column(Text, nullable=False)
+    text = Column(Text, nullable=False)
+    raw_text = Column(Text, nullable=False)
+    strokes = Column(Text, nullable=False)
     created_at = Column(DateTime, nullable=False, default=datetime.now)
-    posted_at = Column(DateTime, nullable=True, default=None)
     user_id = Column(Integer, ForeignKey("users.id"))
 
     user = relationship("User")
 
+class Prompts(Base):
+    __tablename__="prompts"
+    id = Column(Integer, primary_key=True)
+    text = Column(Text, nullable=False)
 
 def create_tables():
     Base.metadata.create_all(engine)
-    u = User(email="test@test.com")
+    u = User(name="Tester", email="test@test.com", occupation="programmer", age=23, right_hand="true")
     u.set_password("unicorn")
     session.add(u)
-    p = Post(title="This is a test post", body="This is the body of a test post.")
-    u.posts.append(p)
+    a = Analytics(text="This is a test post", raw_text="This is the raw_text of a test post.", 
+        strokes="test", user_id=u.id)
+    u.analytics.append(a)
+    session.commit()
+
+def create_prompts(filename):
+    f = open(filename)
+    for line in f:
+        p = Prompts(text=unicode(line))
+        session.add(p)
     session.commit()
 
 if __name__ == "__main__":
     create_tables()
+    create_prompts("prompts.txt")
--- views.py
+++ views.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, redirect, request, g, session, url_for, flash
-from model import User, Post
+from model import User, Analytics, Prompts
 from flask.ext.login import LoginManager, login_required, login_user, current_user
 from flaskext.markdown import Markdown
 import config
 import forms
 import model
+import random
+
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -25,34 +27,66 @@ Markdown(app)
 
 @app.route("/")
 def index():
-    posts = Post.query.all()
-    return render_template("index.html", posts=posts)
+    if session.get('user_id'):
+        user = User.query.filter_by(id=session['user_id']).first()
+    else:
+        user = None
+    return render_template("index.html", user=user)
 
-@app.route("/post/<int:id>")
-def view_post(id):
-    post = Post.query.get(id)
-    return render_template("post.html", post=post)
+@app.route("/analytics")
+def no_analytics():
+    if session.get('user_id'):
+        user = User.query.filter_by(id=session['user_id']).first()
+        analytics = user.analytics
+        if len(analytics) > 0:
+            return render_template("existing_analytics.html", raw_text=analytics[-1].raw_text, text=analytics[-1].text, strokes=analytics[-1].strokes)
+    return render_template("no_analytics.html")
 
-@app.route("/post/new")
-@login_required
-def new_post():
-    return render_template("new_post.html")
+@app.route("/analytics", methods=["POST"])
+def show_analytics():
+    text = request.form.get("final_text")
+    raw_text = request.form.get("rawtext")
+    strokes = request.form.get("stroke")
+    print text
+    print raw_text
+    print strokes
+    if session.get("user_id"):
+        user_id = session['user_id']
+        new_a = Analytics(text=text, raw_text=raw_text, strokes=strokes, user_id=user_id)
+        model.session.add(new_a)
+        model.session.commit()
+    else:
+        user_id=None
+    return render_template("analytics.html", strokes=strokes, text=text, raw_text=raw_text, user_id=user_id)
 
-@app.route("/post/new", methods=["POST"])
-@login_required
-def create_post():
-    form = forms.NewPostForm(request.form)
-    if not form.validate():
-        flash("Error, all fields are required")
-        return render_template("new_post.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
-    post = Post(title=form.title.data, body=form.body.data)
-    current_user.posts.append(post) 
-    
-    model.session.commit()
-    model.session.refresh(post)
-
-    return redirect(url_for("view_post", id=post.id))
+@app.route("/register", methods=["POST"])
+def register_newuser():
+    name = request.form.get("name")
+    age = request.form.get("age")
+    occupation = request.form.get("occupation")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    v_password = request.form.get("verify_password")
+    right_hand = request.form.get("right_hand")
+    print "right handed: ", type(right_hand)
+    user = User.query.filter_by(email=email).first()
+    if user == None:
+        if password == v_password:
+            flash("New user created")
+            new_user = User(email=email, right_hand=right_hand, occupation=occupation, age=age, name=name)
+            new_user.set_password(password)
+            model.session.add(new_user)
+            model.session.commit()
+            return redirect(url_for("login"))
+        else:
+            flash("Passwords do not match")
+            return redirect(url_for("register"))
+    flash("User email already exists.")
+    return redirect(url_for("register"))
 
 @app.route("/login")
 def login():
@@ -75,8 +109,47 @@ def authenticate():
         return render_template("login.html")
 
     login_user(user)
+    session['user_id'] = user.id
     return redirect(request.args.get("next", url_for("index")))
 
+@app.route("/pekl/<user_id>")
+def view_pekl(user_id):
+    return render_template("pekl.html")
+
+@app.route("/keyboard", methods=["POST"])
+def continue_keyboard():
+    prompt = Prompts.query.get(random.randint(1, 238))
+    # strokes = request.form.get("stroke")
+    # raw = request.form.get("rawtext")
+    # final = request.form.get("final_text")
+    return render_template("keyboard.html", prompt=prompt.text)
+
+@app.route("/keyboard")
+def show_keyboard():
+    prompt = Prompts.query.get(random.randint(1, 238))
+    return render_template("keyboard.html", prompt=prompt.text)
+
+@app.route("/allkeyboards")
+def all_keyboards():
+    return render_template("construction.html")
+
+@app.route("/typingtest")
+def typing_test():
+    return render_template("construction.html")
+
+@app.route("/remap")
+def remap_keys():
+    return render_template("construction.html")
+
+@app.route("/logging")
+def log_keys():
+    return render_template("construction.html")
+
+@app.route("/signout")
+def signout():
+    session.clear()
+    flash("User signed out.")
+    return redirect(url_for("login"))
 
 if __name__ == "__main__":
     app.run(debug=True)
