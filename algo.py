a04c4aafbd7d53ef1ce705016aefbdf4c39d162b
dslevi <dslevi12@gmail.com>
2013-11-08 18:38:00 -0800

    successfully creates keyboard according to dvorak principles

--- algo.py
+++ algo.py
@@ -1,21 +1,3 @@
-keycode = { '20': 'CAPS', '8': 'DELETE', '120': 'x', '121': 'y', '122': 'z', '123': '{', '124': '|', '125': '}', '126': '~', '118': 'v', '59': ';', 
-            '58': ';', '55': '7', '54': '6', '57': '9', '56': '8', '51': '3', '50': '2', '53': '5', '52': '4', 'x': 'META', '115': 's', '114': 'r', 
-            '117': 'u', '116': 't', '111': 'o', '110': 'n', '113': 'q', '112': 'p', '82': 'R', '83': 'S', '80': 'P', '81': 'Q', '119': 'w', '87': 'W', 
-            '84': 'T', '85': 'U', '108': 'l', '109': 'm', '102': 'f', '103': 'g', '100': 'd', '101': 'e', '106': 'j', '107': 'k', '104': 'h',
-            '105': 'i', '39': "'", '38': '&', '33': '!', '32': 'SPACE', '37': '%', '36': '$', '35': '#', '34': '"', '60': '<', '61': '=', '62': '>', 
-            '63': '?', '64': '@', '65': 'A', '66': 'B', '67': 'C', '68': 'D', '69': 'E', '99': 'c', '98': 'b', '91': '[', '90': 'Z', '93': ']',
-             '92': '\\', '95': '_', '94': '^', '97': 'a', '96': '`', '13': 'ENTER', '17': 'CTRL', '16': 'SHIFT', '18': 'ALT', '88': 'X', '89': 'Y', 
-             '48': '0', '49': '1', '46': '.', '86': 'V', '44': ',', '45': '-', '42': '*', '43': '+', '40': '(', '41': ')', '9': 'TAB', '77': 'M', 
-             '76': 'L', '75': 'K', '74': 'J', '73': 'I', '72': 'H', '71': 'G', '70': 'F', '79': 'O', '78': 'N', '47': '/'}
-
-qwerty_map = { 126:'A1', 96:'A1', 33:'A2', 49:'A2', 64:'A3', 50:'A3', 35:'A4', 51:'A4', 36:'A5', 52:'A5', 37:'A6', 53:'A6', 94:'A7', 54:'A7', 38:'A8', 
-                55:'A8', 42:'A9', 56:'A9', 40:'A10', 57:'A10', 41:'A11', 48:'A11', 95:'A12', 45:'A12', 43:'A13', 61:'A13', 8:'A14', 
-                9:'B1', 81:'B2', 113:'B2', 87:'B3', 119:'B3', 69:'B4', 101:'B4', 82:'B5', 114:'B5', 84:'B6', 116:'B6', 89:'B7', 121:'B7', 85:'B8', 117:'B8', 
-                73:'B9', 105:'B9', 79:'B10', 111:'B10', 80:'B11', 112:'B11', 123:'B12', 91:'B12', 125:'B13', 93:'B13', 124:'B14', 92:'B14', 
-                20:'C1', 65:'C2', 97:'C2', 83:'C3', 115:'C3', 68:'C4', 100:'C4', 70:'C5', 102:'C5', 71:'C6', 103:'C6', 72:'C7', 104:'C7', 74:'C8', 106:'C8', 
-                75:'C9', 107:'C9', 76:'C10', 108:'C10', 58:'C11', 59:'C11', 34:'C12', 39:'C12', 13:'C13', 16:'D1', 90:'D2', 122:'D2', 88:'D3', 120:'D3', 
-                67:'D4', 99:'D4', 86:'D5', 118:'D5', 66:'D6', 98:'D6', 78:'D7', 110:'D7', 77:'D8', 109:'D8', 60:'D9', 44:'D9', 62:'D10', 46:'D10', 63:'D11', 
-                47:'D11', 16:'D12', 17:'E1', 18:'E3', 32:'E4', 18:'E5', 17:'E8'}
 
 #timestamps stripped out, metadata can be recycled later or removed later if necessary
 def findStrokes(strokes):
@@ -50,41 +32,76 @@ def findKeyWeights(strokes, layout):
         weights[key] = weights.get(key, 0) + 1
     return weights
 
-#by key value instead of location, different values for upper/lowercase letters
-#figure out what to do with shifts later
-def findBigrams(keystrokes):
-    bigrams = []
-    for i in range(len(keystrokes)):
-        if (i + 1) < len(keystrokes):
-            bigrams.append((keystrokes[i], keystrokes[i + 1]))
-    return bigrams
-
 def findBigramFreq(bigrams):
     freq = {}
     for bigram in bigrams:
         freq[bigram] = freq.get(bigram, 0) + 1
     return freq
 
-def placeKeys(bigram_freq):
-    keyboard = []
+def placeKeys(strokes):
+    bigram_freq = findBigramFreq(findBigrams(findStrokes(strokes)))
+
+    keys = {96:"tilde", 126:"tilde", 33:"one", 49:"one", 50:"two", 64:"two", 35:"three", 
+        51:"three", 52:"four", 53:"five", 37:"five", 54:"six", 94:"six", 55:"seven", 
+        38:"seven", 42:"eight", 56:"eight", 57:"nine", 40:"nine", 48:"zero", 41:"zero", 
+        36:"four", 45:"dash", 95:"dash", 43:"plus", 61:"plus", 8:"delete", 9:"tab", 
+        13:"enter", 16:"shift", 17:"control", 18:"alt", 20:"caps",
+        32:"space", 97:"A", 65:"A", 113:"Q", 81:"Q", 119:"W", 87:"W", 101:"E", 69:"E", 114:"R",
+        82:"R", 116:"T", 84:"T", 89:"Y", 121:"Y", 85:"U", 117:"U", 73:"I", 105:"I", 80:"P", 
+        112:"P", 123:"brace_open", 91:"brace_open", 93:"brace_close", 125:"brace_close", 
+        124:"pipe", 92:"pipe", 111:"O", 79:"O", 98:"B", 66:"B", 115:"S", 83:"S", 100:"D", 
+        68:"D", 102:"F", 70:"F", 103:"G", 71:"G", 104:"H", 72:'H', 74:'J', 106:'J', 107:'K', 
+        75:'K', 76:'L', 108:'L', 77:'M', 109:'M', 59:"colon", 58:'colon', 39:'quote', 
+        34:'quote', 122:"Z", 90:"Z", 120:"X", 88:"X", 99:"C", 67:"C", 118:"V", 86:"V", 110:"N",
+        78:"N", 60:"comma", 44:"comma", 62:"period", 46:"period", 63:"question", 47:"question"}
+
+    common = ['space', "E", "T", "shift", 'A', 'O', 'I', 'N', 'S', 'R', 'H', 'delete', 'L', 'D', 'C', 'U', 'enter', 'M', 'F', 'P', 'G', 'W', 'Y', 'B', 'comma', 'period', 'V',
+            'K', 'nine', 'zero', 'dash', 'colon', 'quote', 'plus', 'tab', 'X', 'question', 'four', 'eight', 'one', 'J', 'brace_open', 'brace_close', 'Q', "two", 'Z', 'five',
+            'three', 'pipe', 'six', 'seven', 'tilde', "control", "alt", 'caps']
+
+    #Dvorak principles - home row prioritized, bigrams split to different hands
+    bigrams = []
+    for x in bigram_freq:
+        bigrams.append((bigram_freq[x], x))
+    sorted_bigrams = sorted(bigrams, reverse=True)
+
+    dvorak_keyboard = []
+    for x, y in sorted_bigrams:
+        for i in y:
+            if keys[i] not in dvorak_keyboard:
+                dvorak_keyboard.append(keys[i])
+
+    if len(dvorak_keyboard) < len(common):
+        for h in common:
+            if h not in dvorak_keyboard:
+                dvorak_keyboard.append(h)
+    
+    #the corresponding location map, highest priority to lowest| removed D12 - shift, E8 - ctrl, E5 - alt
+    dvorak_map = ['C06', 'C05', 'C07', 'C04', 'C08', 'C03', 'C09', 'C02', 'C10', 'C01', 'C11', 'C12', 'C13', 'B06', 'B05', 'B07', 'B04', 'B08', 'B03', 'B09', 'B02', 'B10', 'B01', 'B11', 
+                'B12', 'B13', 'B14', 'D07', 'D06', 'D08', 'D05', 'D09', 'D04', 'D10', 'D03', 'D11', 'D02', 'D01', 'E04', 'A08', 'A07', 'A06', 'A09', 
+                'A05', 'A10', 'A04', 'A11', 'A03', 'A12', 'A02', 'A13', 'A01', 'A14', 'E03', 'E01']
+
+    keyboard = sorted(zip(dvorak_map, dvorak_keyboard))
 
     return keyboard
 
-# #optimal is defined by vector placement, costs v benefits ...right now just homerow placement of most freq bigrams
-# def find_opt_keyboard(keyboards):
-#     best = []
-#     return best
 
-# def dvorak_priority(keystrokes):
-#     frequencies = findBigramFreq(findBigrams(findStrokes(keystrokes)))
-#     all_keyboards = []
-#     #find all possible unique keyboard layout variations, need to replace .... use genetic algo?
-#     for i in range(100):
-#         keyboard = placeKeys(frequencies)
-#         all_keyboards.append(keyboard)
-#     return find_opt_keyboard(all_keyboards)
 
-#MISC analytics
+
+
+
+
+
+
+
+
+
+s = "103 1383963698624|104 1383963698645|105 1383963699270|106 1383963699341|97 1383963699445|102 1383963699454|115 1383963699525|107 1383963699565|100 1383963699581|102 1383963699669|107 1383963699781|106 1383963699791|104 1383963699885|102 1383963699901|98 1383963700070|110 1383963700342|108 1383963700477|107 1383963700492|120 1383963700733|99 1383963700742|122 1383963700758|118 1383963700862|104 1383963700917|107 1383963701061|106 1383963701077|98 1383963701181|97 1383963701302|106 1383963701365|102 1383963701453|104 1383963701485|97 1383963701661|115 1383963701676|100 1383963701695|104 1383963701703|102 1383963701773|107 1383963701829|100 1383963701941|115 1383963701965|102 1383963702013|106 1383963702109|115 1383963702150|100 1383963702166|108 1383963702221|102 1383963702230|104 1383963702485|101 1383963702565|108 1383963702685|108 1383963702839|111 1383963702963|32 1383963703052|116 1383963703116|104 1383963703157|101 1383963703190|114 1383963703293|101 1383963703365|32 1383963703468|109 1383963703605|121 1383963703766|32 1383963703828|110 1383963703974|97 1383963704029|109 1383963704125|101 1383963704197|32 1383963704244|105 1383963704365|100 1383963704438|32 1383963704540|8 1383963705133|8 1383963705292|115 1383963705629|32 1383963705740|100 1383963705909|97 1383963705965|110 1383963706013|105 1383963706133|101 1383963706173|108 1383963706309|108 1383963706461|101 1383963706493|32 1383963706599|119 1383963706805|104 1383963706885|97 1383963706949|116 1383963707005|32 1383963707060|105 1383963707101|115 1383963707190|32 1383963707276|121 1383963707324|111 1383963707405|117 1383963707502|114 1383963707525|32 1383963707653|110 1383963707813|97 1383963707838|109 1383963707982|101 1383963708021|63 1383963708237|"
+    bigrams = []
+    for i in range(len(keystrokes)):
+        if (i + 1) < len(keystrokes):
+            bigrams.append((keystrokes[i], keystrokes[i + 1]))
+    return bigrams
 
 def ms_to_s(time):
     return float(time)/float(1000)
